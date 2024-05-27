fb577fa32f41a4dd4f84f27104cf9f1c1561e808
Lance Pollard <lp@elk.fm>
2024-05-27 04:34:56 -0700

    save

--- host/node.d.ts
+++
@@ -1,6 +0,0 @@
-import Kink from '@termsurf/kink';
-import makeText from './index.js';
-export * from './index.js';
-export { makeText };
-export declare function makeKinkText(kink: Kink): string;
-export declare function makeBaseKinkText(kink: Error): string;
--- host/test.d.ts
+++
@@ -1,7 +0,0 @@
-import Kink from '@termsurf/kink';
-type Base = {
-    syntax_error: {};
-};
-type Name = keyof Base;
-export default function kink<N extends Name>(form: N, link?: Base[N]): Kink;
-export {};
